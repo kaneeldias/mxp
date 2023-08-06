b0b7135815eafd7b98caac8b955457c319babe09
kaneeldias <kaneeldias@hotmail.com>
2023-08-06 22:43:19 +0530

    Use environment variables instrad of firebase-service-account.json

--- app/init.tsx
+++ app/init.tsx
@@ -1,10 +1,13 @@
 const {initializeApp, cert} = require('firebase-admin/app');
 const {getFirestore} = require('firebase-admin/firestore');
 
-const serviceAccount = require("@/firebase-service-account.json");
 try {
     initializeApp({
-        credential: cert(serviceAccount)
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
+        })
     });
 } catch (err) {
     // we skip the "already exists" message which is
